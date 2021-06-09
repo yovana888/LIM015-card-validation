cb1bfc27d2314957d8527d87e0846df0557f7788
yovana888 <yovana.otaku@gmail.com>
2021-06-09 09:07:43 -0500

    Cierre de Sprint

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
-import validator from './validator.js';
+import { validator } from './validator.js';
 
-console.log(validator);
+function cardFormat() {
+    let numero_temporal = document.getElementById("numero-tarjeta").value;
+    let rawNumbers = numero_temporal.replace(/-/g, '');
+    let cardLength = rawNumbers.length;
+    if (cardLength !== 0 && cardLength <= 12 && cardLength % 4 == 0) {
+        document.getElementById("numero-tarjeta").value = numero_temporal + '-';
+    }
+}
+
+//4137-8947-1175-5904
+function showViewMaskify(maskifyCreditCardNumber) {
+    let hoy = new Date();
+    document.getElementById("text-maskify").innerHTML = maskifyCreditCardNumber;
+    document.getElementById("text-fecha").innerHTML = hoy.getFullYear() + '/' + hoy.getMonth() + '/' + hoy.getDay() + '  ' + hoy.getHours() + ':' + hoy.getMinutes();
+    document.getElementById("view_inicial").style = "display:none";
+    document.getElementById("view_final").style = "visibility: show";
+
+}
+
+document.getElementById("numero-tarjeta").addEventListener("keypress", cardFormat);
+
+document.getElementById("btn-validar").addEventListener("click", function() {
+    let creditCardNumber = document.getElementById("numero-tarjeta").value;
+    let respuesta = validator.isValid(creditCardNumber);
+    switch (respuesta) {
+        case true:
+            let maskifyCreditCardNumber = validator.maskify(creditCardNumber);
+            let typeCreditCard = validator.getTypeCreditCard(creditCardNumber);
+            showViewMaskify(maskifyCreditCardNumber);
+            break;
+        case false:
+            alert('Tarjeta Invalida');
+            break;
+        default:
+            alert('Por favor Verifique sus Datos, recuerde que el Numero de Tarjeta debe contener solo Numeros y de 16 digitos');
+    }
+});
+
+document.getElementById("btn-volver").addEventListener("click", function() {
+    document.getElementById("view_inicial").style = "visibility: show";
+    document.getElementById("view_final").style = "display:none";
+    document.getElementById("numero-tarjeta").value = "";
+});
--- src/validator.js
+++ src/validator.js
@@ -1,6 +1,71 @@
-const validator = {
-    // ...
+function isValid(creditCardNumber) {
 
-};
-console.log('Prueba Commit 2s');
-//export default validator;
+    creditCardNumber = creditCardNumber.replace(/-/g, '');
+    const regex = /^[0-9]*$/;
+    let respuesta = '';
+    /*^ marca el comienzo de la cadena. $ marca el final de la cadena. [0-9] indica un caracter numérico. * indica que se cumpla 0 o más veces. */
+    if (regex.test(creditCardNumber) == true && creditCardNumber.length >= 12) {
+        respuesta = luhn_algorithm(creditCardNumber); //Retorna true o false
+    } else {
+        respuesta = 'error-formato';
+    }
+    return respuesta;
+}
+
+function luhn_algorithm(creditCardNumber) {
+    let array_num_tarjeta = Array.from(creditCardNumber); //Convierte  el numero de tarjeta en un array
+
+    for (let i = 0; i < array_num_tarjeta.length; i++) {
+        if (i % 2 == 0) {
+            let valor_temporal = array_num_tarjeta[i] * 2;
+            array_num_tarjeta[i] = valor_temporal;
+
+            if (valor_temporal > 9) {
+                let array_digitos = Array.from(String(valor_temporal), Number); //Convierte el Numero en un array
+                let new_numero = array_digitos[0] + array_digitos[1];
+                array_num_tarjeta[i] = new_numero;
+            }
+        }
+    }
+
+    let suma = 0;
+
+    //Sumar Digitos del nuevo array 
+    for (let i = 0; i < array_num_tarjeta.length; i++) {
+        suma = suma + parseFloat(array_num_tarjeta[i]);
+    }
+
+    if (suma % 10 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function maskify(creditCardNumber) {
+    creditCardNumber = creditCardNumber.replace(/-/g, '');
+    let maskifyCreditCardNumber = creditCardNumber.replace(/.(?=.{4})/g, "#");
+    return maskifyCreditCardNumber;
+}
+
+function getTypeCreditCard(creditCardNumber) {
+    creditCardNumber = creditCardNumber.replace(/-/g, '');
+    switch (creditCardNumber) {
+        case true:
+            let maskifyCreditCardNumber = validator.maskify(creditCardNumber);
+            let typeCreditCard = validator.getTypeCreditCard(creditCardNumber);
+            showViewMaskify(maskifyCreditCardNumber);
+            break;
+        case false:
+            alert('Tarjeta Invalida');
+            break;
+        default:
+            alert('Por favor Verifique sus Datos, recuerde que el Numero de Tarjeta debe contener solo Numeros y de 16 digitos');
+    }
+}
+
+export const validator = {
+    isValid,
+    maskify,
+    getTypeCreditCard
+}
