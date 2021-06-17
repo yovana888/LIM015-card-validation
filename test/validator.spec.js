a3469b341a6883cba52e3711947311ed3946328a
yovana888 <yovana.otaku@gmail.com>
2021-06-16 21:46:48 -0500

    Test Unit

--- test/validator.spec.js
+++ test/validator.spec.js
@@ -23,6 +23,10 @@ describe('validator', () => {
             expect(validator.isValid("5031-7104-3691-014")).toBe(false);
         });
 
+        it('debería retornar error-formato para "5031-7141-123"', () => {
+            expect(validator.isValid("5031-7141-123")).toBe("error-formato");
+        });
+
 
     });
 
@@ -41,6 +45,36 @@ describe('validator', () => {
 
         it('Debería retornar "######orld" para "helloworld"', () => {
             expect(validator.maskify("helloworld")).toBe("######orld");
+        });
+    });
+
+    describe('validator.getTypeCreditCard', () => {
+        it('debería ser una función', () => {
+            expect(typeof validator.getTypeCreditCard).toBe('function');
+        });
+
+        it('Debería retornar "VISA" para "4835-0812-7735-2152"', () => {
+            expect(validator.getTypeCreditCard("4835-0812-7735-2152")).toBe("VISA");
+        });
+
+        it('Debería retornar "MasterCard" para "5350-3502-8638-8461"', () => {
+            expect(validator.getTypeCreditCard("5350-3502-8638-8461")).toBe("MasterCard");
+        });
+
+        it('Debería retornar "American Express" para "3727-7338-8734-123"', () => {
+            expect(validator.getTypeCreditCard("3727-7338-8734-123")).toBe("American Express");
+        });
+
+        it('Debería retornar "DISCOVER" para "6011-9222-0206-3441"', () => {
+            expect(validator.getTypeCreditCard("6011-9222-0206-3441")).toBe("Discover");
+        });
+
+        it('Debería retornar "JBC" para "3573-3756-0257-128"', () => {
+            expect(validator.getTypeCreditCard("3573-3756-0257-128")).toBe("JBC");
+        });
+
+        it('Debería retornar "Tipo de Tarjeta no Identificada" para "0000-0000-1111-1111"', () => {
+            expect(validator.getTypeCreditCard("0000-0000-1111-1111")).toBe("Tipo de Tarjeta no Identificada");
         });
     });
 });
