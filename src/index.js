5bc3817af6786b0ae87753934a577132fdcdc76f
yovana888 <yovana.otaku@gmail.com>
2021-06-16 20:27:51 -0500

    Actualizacion

--- src/index.js
+++ src/index.js
@@ -1,15 +1,53 @@
-import { validator } from './validator.js';
+import validator from './validator.js';
 
-function cardFormat() {
+const btnEnviar = document.getElementById("btnEnviar");
+
+btnEnviar.addEventListener('click', () => {
+    let creditCardNumber = document.getElementById("numero-tarjeta").value;
+    let respuesta = validator.isValid(creditCardNumber);
+    switch (respuesta) {
+        case true:
+            {
+                let maskifyCreditCardNumber = validator.maskify(creditCardNumber);
+                let typeCreditCard = validator.getTypeCreditCard(creditCardNumber);
+                showViewMaskify(maskifyCreditCardNumber, typeCreditCard);
+                break;
+            }
+        case false:
+            {
+                alert('Tarjeta Invalida');
+                break;
+            }
+        default:
+            {
+                alert('Por favor Verifique sus Datos, recuerde que el Numero de Tarjeta debe contener solo Numeros y de 12 digitos como minimo');
+            }
+
+    }
+});
+
+
+const cardFormat = document.getElementById("numero-tarjeta");
+
+cardFormat.addEventListener('click', () => {
     let numero_temporal = document.getElementById("numero-tarjeta").value;
     let rawNumbers = numero_temporal.replace(/-/g, '');
     let cardLength = rawNumbers.length;
     if (cardLength !== 0 && cardLength <= 12 && cardLength % 4 == 0) {
         document.getElementById("numero-tarjeta").value = numero_temporal + '-';
     }
-}
+});
 
-//4137-8947-1175-5904
+
+const btnVolver = document.getElementById("btnVolver");
+
+btnVolver.addEventListener("click", function() {
+    document.getElementById("formValid").reset();
+    document.getElementById("view_inicial").style = "visibility: show";
+    document.getElementById("view_final").style = "display:none";
+});
+
+
 function showViewMaskify(maskifyCreditCardNumber, typeCreditCard) {
     let hoy = new Date();
     document.getElementById("text-maskify").innerHTML = maskifyCreditCardNumber;
@@ -19,29 +57,3 @@ function showViewMaskify(maskifyCreditCardNumber, typeCreditCard) {
     document.getElementById("view_final").style = "visibility: show";
 
 }
-
-document.getElementById("numero-tarjeta").addEventListener("keypress", cardFormat);
-
-document.getElementById("btn-validar").addEventListener("click", function() {
-    //Cambair valor
-    const creditCardNumber = document.getElementById("numero-tarjeta").value;
-    let respuesta = validator.isValid(creditCardNumber);
-    switch (respuesta) {
-        case true:
-            const maskifyCreditCardNumber = validator.maskify(creditCardNumber);
-            const typeCreditCard = validator.getTypeCreditCard(creditCardNumber);
-            showViewMaskify(maskifyCreditCardNumber, typeCreditCard);
-            break;
-        case false:
-            alert('Tarjeta Invalida');
-            break;
-        default:
-            alert('Por favor Verifique sus Datos, recuerde que el Numero de Tarjeta debe contener solo Numeros y de 12 digitos como minimo');
-    }
-});
-
-document.getElementById("btn-volver").addEventListener("click", function() {
-    document.getElementById("formValid").reset();
-    document.getElementById("view_inicial").style = "visibility: show";
-    document.getElementById("view_final").style = "display:none";
-});
--- src/validator.js
+++ src/validator.js
@@ -1,86 +1,77 @@
-function isValid(creditCardNumber) {
+const validator = {
+    isValid: function(creditCardNumber) {
+        creditCardNumber = creditCardNumber.replace(/-/g, '');
+        const regex = /^[0-9]*$/;
 
-    creditCardNumber = creditCardNumber.replace(/-/g, '');
-    const regex = /^[0-9]*$/;
-    let respuesta = '';
-    /*^ marca el comienzo de la cadena. $ marca el final de la cadena. [0-9] indica un caracter numérico. * indica que se cumpla 0 o más veces. */
-    if (regex.test(creditCardNumber) == true && creditCardNumber.length >= 12) {
-        respuesta = luhn_algorithm(creditCardNumber); //Retorna true o false
-    } else {
-        respuesta = 'error-formato';
-    }
-    return respuesta;
-}
+        let respuesta;
 
-function luhn_algorithm(creditCardNumber) {
-    let array_num_tarjeta = Array.from(creditCardNumber); //Convierte  el numero de tarjeta en un array
+        if (regex.test(creditCardNumber) == true && creditCardNumber.length >= 12) {
+            let array_num_tarjeta = Array.from(creditCardNumber); //Convierte  el numero de tarjeta en un array
+            for (let i = 0; i < array_num_tarjeta.length; i++) {
+                if (i % 2 == 0) {
+                    let valor_temporal = array_num_tarjeta[i] * 2;
+                    array_num_tarjeta[i] = valor_temporal;
 
-    for (let i = 0; i < array_num_tarjeta.length; i++) {
-        if (i % 2 == 0) {
-            let valor_temporal = array_num_tarjeta[i] * 2;
-            array_num_tarjeta[i] = valor_temporal;
-
-            if (valor_temporal > 9) {
-                let array_digitos = Array.from(String(valor_temporal), Number); //Convierte el Numero en un array
-                let new_numero = array_digitos[0] + array_digitos[1];
-                array_num_tarjeta[i] = new_numero;
+                    if (valor_temporal > 9) {
+                        let array_digitos = Array.from(String(valor_temporal), Number); //Convierte el Numero en un array
+                        let new_numero = array_digitos[0] + array_digitos[1];
+                        array_num_tarjeta[i] = new_numero;
+                    }
+                }
             }
+
+            let suma = 0;
+
+            //Sumar Digitos del nuevo array 
+            for (let i = 0; i < array_num_tarjeta.length; i++) {
+                suma = suma + parseFloat(array_num_tarjeta[i]);
+            }
+            respuesta = (suma % 10) == 0 ? true : false;
+
+        } else {
+            respuesta = 'error-formato';
         }
+
+        return respuesta;
+    },
+
+    maskify: function(creditCardNumber) {
+        creditCardNumber = creditCardNumber.replace(/-/g, '');
+        let array_num_tarjeta = Array.from(creditCardNumber);
+        for (let i = 0; i < array_num_tarjeta.length - 4; i++) {
+            array_num_tarjeta[i] = '#';
+        }
+        let maskifyCreditCardNumber = array_num_tarjeta.join("");
+
+        return maskifyCreditCardNumber;
+    },
+
+    getTypeCreditCard: function(creditCardNumber) {
+        creditCardNumber = creditCardNumber.replace(/-/g, '');
+        let visaRegEx = /^4[0-9]{6,}$/; // Las Visas empiezan por 4.
+        let mastercardRegEx = /^5[1-5][0-9]{5,}|222[1-9][0-9]{3,}|22[3-9][0-9]{4,}|2[3-6][0-9]{5,}|27[01][0-9]{4,}|2720[0-9]{3,}$/; // Los números de las MasterCard empezaban del  51 al 55 o 222100-272099.
+        let amexpRegEx = /^3[47][0-9]{5,}$/; //Los números de American Express empiezan por 34 o 37.
+        let discovRegEx = /^6(?:011|5[0-9]{2})[0-9]{3,}$/; //Discover empiezan por 6011 o 65.
+        let jbcRexEx = /^(?:2131|1800|35[0-9]{3})[0-9]{3,}$/; //JCB empiezan por  2131, 1800 o 35.
+        let typeCreditCard;
+
+        if (visaRegEx.test(creditCardNumber)) {
+            typeCreditCard = 'VISA';
+        } else if (mastercardRegEx.test(creditCardNumber)) {
+            typeCreditCard = 'MasterCard';
+        } else if (amexpRegEx.test(creditCardNumber)) {
+            typeCreditCard = 'American Express';
+        } else if (discovRegEx.test(creditCardNumber)) {
+            typeCreditCard = 'Discover';
+        } else if (jbcRexEx.test(creditCardNumber)) {
+            typeCreditCard = 'JBC';
+        } else {
+            typeCreditCard = 'Tipo de Tarjeta no Identificada';
+        }
+
+        return typeCreditCard;
     }
 
-    let suma = 0;
+};
 
-    //Sumar Digitos del nuevo array 
-    for (let i = 0; i < array_num_tarjeta.length; i++) {
-        suma = suma + parseFloat(array_num_tarjeta[i]);
-    }
-
-    if (suma % 10 == 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function maskify(creditCardNumber) {
-    creditCardNumber = creditCardNumber.replace(/-/g, '');
-    let array_num_tarjeta = Array.from(creditCardNumber);
-    for (let i = 0; i < array_num_tarjeta.length - 4; i++) {
-        array_num_tarjeta[i] = '#';
-    }
-    let maskifyCreditCardNumber = array_num_tarjeta.join("");
-    return maskifyCreditCardNumber;
-}
-
-function getTypeCreditCard(creditCardNumber) {
-    creditCardNumber = creditCardNumber.replace(/-/g, '');
-    let visaRegEx = /^4[0-9]{6,}$/; // Las Visas empiezan por 4.
-    let mastercardRegEx = /^5[1-5][0-9]{5,}|222[1-9][0-9]{3,}|22[3-9][0-9]{4,}|2[3-6][0-9]{5,}|27[01][0-9]{4,}|2720[0-9]{3,}$/; // Los números de las MasterCard empezaban del  51 al 55 o 222100-272099.
-    let amexpRegEx = /^3[47][0-9]{5,}$/; //Los números de American Express empiezan por 34 o 37.
-    let discovRegEx = /^6(?:011|5[0-9]{2})[0-9]{3,}$/; //Discover empiezan por 6011 o 65.
-    let jbcRexEx = /^(?:2131|1800|35[0-9]{3})[0-9]{3,}$/; //JCB empiezan por  2131, 1800 o 35.
-    let typeCreditCard;
-
-    if (visaRegEx.test(creditCardNumber)) {
-        typeCreditCard = 'VISA';
-    } else if (mastercardRegEx.test(creditCardNumber)) {
-        typeCreditCard = 'MasterCard';
-    } else if (amexpRegEx.test(creditCardNumber)) {
-        typeCreditCard = 'American Express';
-    } else if (discovRegEx.test(creditCardNumber)) {
-        typeCreditCard = 'Discover';
-    } else if (jbcRexEx.test(creditCardNumber)) {
-        typeCreditCard = 'JBC';
-    } else {
-        typeCreditCard = 'Tipo de Tarjeta no Identificada';
-    }
-
-    return typeCreditCard;
-
-}
-
-export const validator = {
-    isValid,
-    maskify,
-    getTypeCreditCard
-}
+export default validator;
